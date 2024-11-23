b5d30ecb08d5ca1b3d1bfd4702c6155aba09716f
caizw18 <163475183+caizw18@users.noreply.github.com>
2024-11-23 10:14:13 -0500

    Add files via upload

---
+++ value.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    int x,y,z,m,n;
+
+    x=y=z=m=n=10;
+    return 0;
+}
---
+++ whileloop.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 0;
+
+    while (i < 10)
+    {
+        printf("i is now %d!\n",i);
+        i++;
+    }
+    
+    printf("All done!\n");
+
+    return 0;
+}
